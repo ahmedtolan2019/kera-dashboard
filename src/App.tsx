0235c27584471d2099fc8f97a453bb231e953cac
ahmed tolan <Ahmed.Tolan@sh-eng.menofia.edu.eg>
2022-12-03 00:23:24 +0200

    Init and customize tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./index.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">kera- dashboard</div>;
+  return (
+    <div className="min-w-screen min-h-screen bg-colorful-circles bg-cover bg-center bg-no-repeat font-kera text-kera-blue-900 bg-blend-lighten">
+      <div className="min-w-screen min-h-screen bg-kera-white/90 backdrop-blur-lg">
+        Kera Dashboard | كيرا داشبورد
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,34 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "colorful-circles": "url('/src/assets/images/circle-bg-img-10.png')",
+      },
+      colors: {
+        kera: {
+          white: "#FCFDFD",
+          blue: {
+            400: "#1793DE",
+            900: "#152D4A",
+          },
+          yellow: {
+            400: "#CCD36A",
+            600: "#FFBB3B",
+          },
+          pink: "#E65AC6",
+          gray: {
+            100: "#EFFCFF",
+            300: "#E7EDF3",
+            500: "#D2DCE8",
+            700: "#5881A4",
+          },
+        },
+      },
+      fontFamily: {
+        kera: ["Almarai", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
